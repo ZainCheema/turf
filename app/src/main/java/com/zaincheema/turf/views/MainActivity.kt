ccd2f9d3c3c88d13c186acdaf94271362cdc8581
Zain Cheema <zaincheema96@gmail.com>
2021-04-23 20:01:34 +0100

    Compose now working

--- app/src/main/java/com/zaincheema/turf/views/MainActivity.kt
+++ app/src/main/java/com/zaincheema/turf/views/MainActivity.kt
@@ -2,12 +2,14 @@ package com.zaincheema.turf.views
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.core.app.ComponentActivity
 
 @SuppressLint("RestrictedApi")
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
