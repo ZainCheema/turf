082322271825764f9275cfb3ceb1a5e5d5331955
Zain Cheema <zaincheema96@gmail.com>
2021-05-10 17:43:53 +0100

    WIP color change, made changes to API

--- app/src/main/java/com/zaincheema/turf/TurfApiService.kt
+++ app/src/main/java/com/zaincheema/turf/TurfApiService.kt
@@ -1,17 +1,21 @@
 package com.zaincheema.turf
 
+import com.squareup.moshi.Json
 import com.zaincheema.turf.model.TurfBox
 import io.reactivex.rxjava3.core.Observable
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface TurfApiService {
-    @GET("/api/v1/boxes/all")
+    @GET("/api/v1/boxes/")
     fun getTurfBoxes(): Observable<List<TurfBox>>
+
+    @PUT("/api/v1/boxes/{id}")
+    fun updateTurfBoxColor(@Path("id") id: Int, @Body tb: TurfBox): Observable<ResponseBody>
 
     companion object {
        private const val API_URL = "https://turf-api-nzzq3nbe4a-uc.a.run.app/"
