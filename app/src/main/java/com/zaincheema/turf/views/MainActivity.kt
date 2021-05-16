9e1084eb3a2164c70bac7b810d8dbbfee7d967b7
Zain Cheema <zaincheema96@gmail.com>
2021-05-16 23:10:22 +0100

    Auto stash before checking out "HEAD"

--- app/src/main/java/com/zaincheema/turf/views/MainActivity.kt
+++ app/src/main/java/com/zaincheema/turf/views/MainActivity.kt
@@ -6,24 +6,30 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.*
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.vanpra.composematerialdialogs.MaterialDialog
+import com.vanpra.composematerialdialogs.color.ColorPalette
+import com.vanpra.composematerialdialogs.color.colorChooser
 import com.zaincheema.turf.TurfApiService
 import com.zaincheema.turf.model.TurfBox
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
-import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -34,6 +40,9 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG: String = "MainActivity.kt"
     private lateinit var turfBoxes: List<TurfBox>
+    private var compositeDisposable = CompositeDisposable()
+    private val service = TurfApiService.retrofit.create<TurfApiService>()
+
     private val colors: List<Color> = listOf(
         Color(0xFF000000),
         Color(0xFF888888),
@@ -44,10 +53,8 @@ class MainActivity : AppCompatActivity() {
         Color(0xFFFFFF00),
         Color(0xFF00FFFF),
     )
-    private var compositeDisposable = CompositeDisposable()
 
-    private val service = TurfApiService.retrofit.create<TurfApiService>()
-
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -59,7 +66,6 @@ class MainActivity : AppCompatActivity() {
         )
 
         supportActionBar?.hide()
-
     }
 
     override fun onDestroy() {
@@ -67,6 +73,7 @@ class MainActivity : AppCompatActivity() {
         compositeDisposable.dispose()
     }
 
+    @ExperimentalAnimationApi
     private fun onGetSuccess(response: List<TurfBox>) {
         Log.d(TAG, "Connected to API, loaded in Turf Boxes")
         Log.d(TAG, response.toString())
@@ -85,42 +92,68 @@ class MainActivity : AppCompatActivity() {
 
     private fun handleBoxClick(tb: TurfBox) {
         Toast.makeText(this, "ayo im box ${tb.id}", Toast.LENGTH_LONG).show()
+        service.updateTurfBoxColor(tb.copy(id = tb.id, colorHex = 0xFFFFFF00))
+            .enqueue(object : Callback<TurfBox> {
+                override fun onResponse(call: Call<TurfBox>, response: Response<TurfBox>) {
+                    Log.d(TAG, response.body().toString())
+                }
 
+                override fun onFailure(call: Call<TurfBox>, t: Throwable) {
+                    Log.e(TAG, "Color change failed :(")
+                    Log.d(TAG, t.toString())
+                }
 
-        service.updateTurfBoxColor(tb.copy(id = tb.id, colorHex = 0xFFFFFF00)).enqueue(object : Callback<TurfBox> {
-            override fun onResponse(call: Call<TurfBox>, response: Response<TurfBox>) {
-                Log.d(TAG, response.body().toString())
-            }
-
-            override fun onFailure(call: Call<TurfBox>, t: Throwable) {
-                Log.e(TAG, "Color change failed :(")
-                Log.d(TAG, t.toString())
-            }
-
-        })
+            })
     }
 
+    @ExperimentalAnimationApi
     @OptIn(ExperimentalFoundationApi::class)
     @Composable
     fun TurfMap() {
-        Column() {
+        var showChooser = remember { mutableStateOf(false) }
+        Column {
             TopAppBar(title = { Text(text = "turf") }, backgroundColor = Color.White)
             LazyVerticalGrid(
                 cells = GridCells.Fixed(10),
                 modifier = Modifier
-                    .fillMaxSize()
                     .background(Color.Transparent)
                     .padding(8.dp)
             ) {
                 items(turfBoxes.size) { t ->
                     Box(
                         Modifier
-                            .clickable(true, null, null) { handleBoxClick(turfBoxes[t]) }
+                            .clickable(true, null, null) {
+                                handleBoxClick(turfBoxes[t])
+                                showChooser.value = true
+                            }
                             .aspectRatio(1f)
                             .background(Color(turfBoxes[t].colorHex))
                     )
                 }
             }
+            // BowWithConstraints will provide the maxWidth used below
+            BoxWithConstraints(modifier = Modifier.fillMaxWidth()) {
+                // LazyRow to display your items horizontally
+                LazyRow(
+                    modifier = Modifier.fillMaxWidth(),
+                    state = rememberLazyListState()
+                ) {
+                    items(colors) { item ->
+                        Box(
+                            modifier = Modifier
+                                //.size(16.dp)
+                                .scale(0.75f)
+                                .aspectRatio(1f)
+                                .padding(8.dp)
+                                .background(item)
+                        ) {
+                            //Text(item) // card's content
+                        }
+                    }
+                }
+            }
         }
     }
 }
+
+
